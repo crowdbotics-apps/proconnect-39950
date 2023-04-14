65fe6379c95632de1e7ce31d3baa45d81da2c80b
Crowdbotics <team@crowdbotics.com>
2023-04-14 21:10:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proconnect-39950",
+  url: "https://proconnect-39950.botics.co",
+  api: "https://proconnect-39950.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
